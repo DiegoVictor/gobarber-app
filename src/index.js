ec47d7b01db4a2ed3cb47e632d78054f26101173
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-09-11 10:31:38 -0300

    Create react-native app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import Test from '~/Teste';
+
+// import { Container } from './styles';
+
+export default function App() {
+  return (
+    <View />
+  );
+}
