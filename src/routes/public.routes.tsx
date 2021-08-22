596da390d8f79854d2fac8c3330ff5902018f306
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-08-22 14:37:40 -0300

    Create public routes file

---
+++ src/routes/public.routes.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import ForgotPassword from '../pages/ForgotPassword';
+
+const Auth = createStackNavigator();
+
+const PublicRoutes: React.FC = () => {
+  return (
+    <Auth.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: { backgroundColor: '#312e38' },
+      }}
+    >
+      <Auth.Screen name="SignIn" component={SignIn} />
+      <Auth.Screen name="SignUp" component={SignUp} />
+      <Auth.Screen name="ForgotPassword" component={ForgotPassword} />
+    </Auth.Navigator>
+  );
+};
+
+export default PublicRoutes;
