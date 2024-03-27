20a26f469fecb6c89aeb16a1d4a0183937067eb4
Diego Victor <diegovictorgonzaga@gmail.com>
2024-03-27 09:07:49 -0300

    Wrap with gesture handler root view

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import 'react-native-gesture-handler';
-
 import React from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import PublicRoute from './routes';
 import AppProvider from './hooks';
 
 const src: React.FC = () => {
   return (
-    <NavigationContainer>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="#312e38"
-        translucent
-      />
-      <AppProvider>
-        <View style={{ backgroundColor: '#312e38', flex: 1 }}>
-          <PublicRoute />
-        </View>
-      </AppProvider>
-    </NavigationContainer>
+    <GestureHandlerRootView>
+      <NavigationContainer>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="#312e38"
+          translucent
+        />
+        <AppProvider>
+          <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+            <PublicRoute />
+          </View>
+        </AppProvider>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 };
 
