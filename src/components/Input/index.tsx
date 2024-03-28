08fd6f5bc57bae9f826cde9521f2b279904128b0
Diego Victor <diegovictorgonzaga@gmail.com>
2024-03-28 10:15:52 -0300

    Migrate Input component to typescript version

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,42 @@
+import React, {
+  useEffect,
+  useRef,
+  forwardRef,
+  PropsWithChildren,
+  useCallback,
+  ForwardRefRenderFunction,
+} from 'react';
+import { TextInputProps, TextInput as RNTextInput } from 'react-native';
+
+import { Container, TextInput, Icon, Error } from './styles';
+
+interface InputProps extends TextInputProps {
+  name: string;
+  icon: string;
+  error?: string;
+  styles?: {};
+}
+
+const Input: ForwardRefRenderFunction<RNTextInput, PropsWithChildren<InputProps>> = (
+  { error, icon, styles, ...props },
+  ref
+) => {
+  return (
+    <Container isErrored={!!error} style={styles}>
+      <Icon
+        name={icon}
+        size={20}
+        color="#666360"
+      />
+      <TextInput
+        ref={ref}
+        placeholderTextColor="#666360"
+        keyboardAppearance="dark"
+        {...props}
+      />
+      {error && <Error>{error}</Error>}
+    </Container>
+  );
+};
+
+export default forwardRef(Input);
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,44 @@
+import styled, { css } from 'styled-components/native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import { TextInput as RNTextInput } from 'react-native';
+
+interface ContainerProps {
+  isErrored: boolean;
+}
+
+export const Container = styled.View<ContainerProps>`
+  align-items: center;
+  background-color: #232129;
+  border: 2px solid #232129;
+  border-radius: 10px;
+  flex-direction: row;
+  height: 60px;
+  margin-top: 8px;
+  padding: 0px 16px;
+  width: 100%;
+
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+`;
+
+export const TextInput = styled(RNTextInput)`
+  color: #fff;
+  flex: 1;
+  font-family: 'RobotoSlab-Regular';
+  font-size: 16px;
+`;
+
+export const Error = styled.Text`
+  color: #c53030;
+  flex: 1;
+  font-family: 'RobotoSlab-Regular';
+  font-size: 12px;
+  text-align: right;
+`;
+
+export const Icon = styled(FeatherIcon)`
+  margin-right: 16px;
+`;
