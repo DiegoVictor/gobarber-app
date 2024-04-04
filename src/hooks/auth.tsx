95b8fb346c8f4f3b236c2d41fab783824e47e40a
Diego Victor <diegovictorgonzaga@gmail.com>
2024-04-04 09:41:54 -0300

    Update auth hook

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -27,7 +27,7 @@ interface Credentials {
   password: string;
 }
 
-interface AuthContextData {
+export interface AuthContextData {
   user: User;
   loading: boolean;
   signIn(credentials: Credentials): Promise<void>;
@@ -56,6 +56,7 @@ export const AuthProvider: React.FC<PropsWithChildren> = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
     api.defaults.headers.authorization = `Bearer ${token}`;
     setData({ token, user });
   }, []);
@@ -68,7 +69,7 @@ export const AuthProvider: React.FC<PropsWithChildren> = ({ children }) => {
         user,
       });
     },
-    [setData, data.token],
+    [data.token],
   );
 
   useEffect(() => {
